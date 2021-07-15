628e176a31c2dddd095468f0eb285924c4eb673c
theo <krueger.theo@gmail.com>
2021-07-15 16:43:22 -0400

    complete restructuring hell of config

--- README.md
+++ README.md
@@ -46,17 +46,23 @@ default modkey is alt
 
 ### **windowing**
 
-| key                    | function                    | requires |
-| ---------------------- | --------------------------- | -------- |
-| mod-6                  | same as mod-0               | n/a      |
-| mod-shift-m            | toggle fullscreen           | n/a      |
-| mod-shift-leftbracket  | scale display to 1024x768*  | xrandr   |
-| mod-shift-rightbracket | scale display to 1280x720*  | xrandr   |
-| mod-shift-backslash    | scale display to 1920x1080* | xrandr   |
-| mod-shift-apostrophe   | scale display to 960x540*   | xrandr   |
-| mod-shift-semiolon     | scale display to 640x480*   | xrandr   |
+| key         | function          | requires |
+| ----------- | ----------------- | -------- |
+| mod-6       | same as mod-0     | n/a      |
+| mod-shift-m | toggle fullscreen | n/a      |
 
-*requires setting display string in config.h
+### **resolution**
+
+| key                    | function                   | requires |
+| ---------------------- | -------------------------- | -------- |
+| mod-shift-leftbracket  | scale display to 1024x768  | xrandr   |
+| mod-shift-rightbracket | scale display to 1280x720  | xrandr   |
+| mod-shift-backslash    | scale display to 1920x1080 | xrandr   |
+| mod-shift-apostrophe   | scale display to 960x540   | xrandr   |
+| mod-shift-semiolon     | scale display to 640x480   | xrandr   |
+
+requires setting display string and resolution in config.h, additional options for scaling mode (nearest or bilinear)
+current 
 
 ### **applications**
 
--- config.h
+++ config.h
@@ -1,23 +1,15 @@
 /* See LICENSE file for copyright and license details. */
 
-/* XF86XK binds from https://cgit.freedesktop.org/xorg/proto/x11proto/tree/XF86keysym.h */
-#define XF86XK_AudioNext        0x1008FF17
-#define XF86XK_AudioPrev        0x1008FF16
-#define XF86XK_AudioPlay        0x1008FF14
-#define XF86XK_AudioRaiseVolume 0x1008FF13
-#define XF86XK_AudioLowerVolume 0x1008FF11
-#define XF86XK_AudioMute        0x1008FF12
-
 /* appearance */
-static const unsigned int borderpx       = 0;        /* border pixel of windows */
-static const unsigned int snap           = 16;       /* snap pixel */
-static const unsigned int systraypinning = 0;        /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft  = 1;   	 /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;        /* systray spacing */
-static const int systraypinningfailfirst = 1;        /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray             = 1;        /* 0 means no systray */
-static const int showbar                 = 1;        /* 0 means no bar */
-static const int topbar                  = 1;        /* 0 means bottom bar */
+static const unsigned int borderpx       = 0;  /* border pixel of windows */
+static const unsigned int snap           = 8;  /* snap pixel */
+static const unsigned int systraypinning = 1;  /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft  = 0;  /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;  /* systray spacing */
+static const int systraypinningfailfirst = 1;  /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray             = 1;  /* 0 means no systray */
+static const int showbar                 = 1;  /* 0 means no bar */
+static const int topbar                  = 1;  /* 0 means bottom bar */
 static const char *fonts[]               = { "monospace:size=10" };
 static const char dmenufont[]            = "monospace:size=10";
 static const char col_gray1[]            = "#222222";
@@ -39,9 +31,8 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	/* { "Gimp",     NULL,       NULL,       0,            1,           -1 }, */
-    { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class         instance      title   tags mask     isfloating   monitor */
+    
 };
 
 /* layout(s) */
@@ -68,65 +59,72 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-/* programs */
-static const char *termcmd[]     = { "urxvt", NULL };    // bound to mod-shift-enter
-static const char *pmcmd[]       = { "palemoon", NULL }; // bound to mod-shift-f
-static const char *fmcmd[]       = { "pcmanfm", NULL };  // bound to mod-shift-g
-static const char *ccmd[]        = { "code", NULL };     // bound to mod-shift-e
-static const char *dccmd[]       = { "discord", NULL };  // bound to mod-shift-d
-static const char *stcmd[]       = { "steam", NULL };    // bound to mod-shift-s
-/* display scaling */
-#define PRIMARYDISPLAYSTRING     "DVI-D-0" // display to apply scaling keybinds to
-#define DISPLAYFILTERTYPE        "nearest" // change display scaling filtering. valid types: nearest,bilinear
-static const char *scale1080[]   = { "xrandr", "--output", PRIMARYDISPLAYSTRING, "--scale-from", "1920x1080", "--filter", DISPLAYFILTERTYPE, NULL }; // bound to mod-shift-backslash
-static const char *scale720[]    = { "xrandr", "--output", PRIMARYDISPLAYSTRING, "--scale-from", "1280x720", "--filter", DISPLAYFILTERTYPE, NULL };  // bound to mod-shift-leftbracket
-static const char *scale540[]    = { "xrandr", "--output", PRIMARYDISPLAYSTRING, "--scale-from", "960x540", "--filter", DISPLAYFILTERTYPE, NULL };   // bound to mod-shift-quote
-static const char *scale768[]    = { "xrandr", "--output", PRIMARYDISPLAYSTRING, "--scale-from", "1024x768", "--filter", DISPLAYFILTERTYPE, NULL };  // bound to mod-shift-rightbracket
-static const char *scale480[]    = { "xrandr", "--output", PRIMARYDISPLAYSTRING, "--scale-from", "640x480", "--filter", DISPLAYFILTERTYPE, NULL };   // bound to mod-shift-semicolon
+// note that due to my laziness this is actually suboptimal, having commands execute shell scripts instead would entail more extensibility and ease of modification.
+/* applications */
+static const char *app_terminal[]    = { "urxvt", NULL };    // bound to mod-shift-enter
+static const char *app_webbrowser[]  = { "palemoon", NULL }; // bound to mod-shift-f
+static const char *app_filemanager[] = { "pcmanfm", NULL };  // bound to mod-shift-g
+static const char *app_editor[]      = { "code", NULL };     // bound to mod-shift-e
+static const char *app_messenger[]   = { "discord", NULL };  // bound to mod-shift-d
+static const char *app_gamestore[]   = { "steam", NULL };    // bound to mod-shift-s
 /* screenshot */
-#define IMAGESAVELOCATION        "/home/theo/Pictures/screenshots/%y-%m-%d-%H:%M:%S.png"           // save location, im too lazy to make it per user so have fun saving it to /home/theo/
-#define VIDEOSAVELOCATION        "/home/theo/Pictures/screenshots/captures/%y-%m-%d-%H:%M:%S.webm" // escrotum is dumb and requires a .webm extension to save at all.
-static const char *sscmd[]       = { "escrotum", IMAGESAVELOCATION, NULL };             // bound to mod-prtsc
-static const char *ssscmd[]      = { "escrotum", IMAGESAVELOCATION, "-s", NULL };       // bound to mod-shift-prtsc
-static const char *sssccmd[]     = { "escrotum", "-s", "-C", NULL };                    // bound to mod-ctrl-prtsc
-static const char *ssrscmd[]     = { "escrotum", VIDEOSAVELOCATION, "-s", "-r", NULL }; // bound to mod-ctrl-shift-prtsc, stop with ctrl-alt-s
+#define IMAGESAVELOCATION "/home/theo/Pictures/screenshots/%y-%m-%d-%H:%M:%S.png"           // save location, lazy edition
+#define VIDEOSAVELOCATION "/home/theo/Pictures/screenshots/captures/%y-%m-%d-%H:%M:%S.webm" // escrotum is dumb and requires a .webm extension to save at all, god i wish i used shell scripts instead of this garbage
+static const char *screenshot_full[]             = { "escrotum", IMAGESAVELOCATION, NULL };             // bound to mod-prtsc
+static const char *screenshot_select[]           = { "escrotum", IMAGESAVELOCATION, "-s", NULL };       // bound to mod-shift-prtsc
+static const char *screenshot_select_clipboard[] = { "escrotum", "-s", "-C", NULL };                    // bound to mod-ctrl-prtsc
+static const char *screenshot_select_video[]     = { "escrotum", VIDEOSAVELOCATION, "-s", "-r", NULL }; // bound to mod-ctrl-shift-prtsc, stop with ctrl-alt-s
 /* media control */
-static const char *pctlnextcmd[] = { "playerctl", "next", NULL };            // bound to media next
-static const char *pctlprevcmd[] = { "playerctl", "previous", NULL };        // bound to media previous
-static const char *pctlppcmd[]   = { "playerctl", "play-pause", NULL };      // bound to media play/pause
-static const char *pctluvolcmd[] = { "playerctl", "volume", "0.05+", NULL }; // bound to media volume up
-static const char *pctldvolcmd[] = { "playerctl", "volume", "0.05-", NULL }; // bound to media volume down
-static const char *mpcmd[]       = { "clementine", NULL };                   // bound to media volume mute
+#define XF86XK_AudioNext                 0x1008FF17
+static const char *media_next[]          = { "playerctl", "next", NULL };            // bound to media next
+#define XF86XK_AudioPrev                 0x1008FF16
+static const char *media_previous[]      = { "playerctl", "previous", NULL };        // bound to media previous
+#define XF86XK_AudioPlay                 0x1008FF14
+static const char *media_playpause[]     = { "playerctl", "play-pause", NULL };      // bound to media play/pause
+#define XF86XK_AudioRaiseVolume          0x1008FF13
+static const char *media_volume_up[]     = { "playerctl", "volume", "0.05+", NULL }; // bound to media volume up
+#define XF86XK_AudioLowerVolume          0x1008FF11
+static const char *media_volume_down[]   = { "playerctl", "volume", "0.05-", NULL }; // bound to media volume down
+#define XF86XK_AudioMute                 0x1008FF12
+static const char *media_launch_player[] = { "clementine", NULL };                   // bound to media volume mute
+/* display scaling */
+// this section is pain
+#define PRIMARYDISPLAYSTRING     "DVI-D-0" // display to apply resolution stuff to
+//static const char *scale1080[]   = { "xrandr", "--output", PRIMARYDISPLAYSTRING, "--scale-from", "1920x1080", "--filter", DISPLAYFILTERTYPE, NULL }; // bound to mod-shift-backslash
 /* default commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]    = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_pink, "-sf", col_gray4, NULL };
+static const char *dmenucmd[]    = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_pink, "-sf", col_gray4, NULL }; // launch dmenu
 
 static Key keys[] = {
 	/* added keys */
-	{ MODKEY|ShiftMask,             XK_Return,                 spawn,          {.v = termcmd } },            // spawn terminal (rxvt-unicode)
-	{ MODKEY|ShiftMask,             XK_f,                      spawn,          {.v = pmcmd } },              // spawn browser (palemoon)
-	{ MODKEY|ShiftMask,             XK_g,                      spawn,          {.v = fmcmd } },              // spawn file manager (pcmanfm)
-	{ MODKEY|ShiftMask,             XK_e,                      spawn,          {.v = ccmd } },               // spawn editor (code)
-	{ MODKEY|ShiftMask,             XK_d,                      spawn,          {.v = dccmd } },              // spawn instant messenger (discord)
-	{ MODKEY|ShiftMask,             XK_s,                      spawn,          {.v = stcmd } },              // spawn game launcher (steam)
-	{ MODKEY,                       XK_Print,                  spawn,          {.v = sscmd } },              // take screenshot of main screen (escrotum)
-	{ MODKEY|ShiftMask,             XK_Print,                  spawn,          {.v = ssscmd } },             // take screenshot with selection (escrotum)
-	{ MODKEY|ControlMask,           XK_Print,                  spawn,          {.v = sssccmd } },            // take screenshot with selection and only copy to clipboard (escrotum)
-	{ MODKEY|ControlMask|ShiftMask, XK_Print,                  spawn,          {.v = ssrscmd } },            // record screen selection, stop with ctl ctrl-alt-s (escrotum)
-	{ 0,                            XF86XK_AudioNext,          spawn,          {.v = pctlnextcmd } },        // media player next entry (playerctl)
-	{ 0,                            XF86XK_AudioPrev,          spawn,          {.v = pctlprevcmd } },        // media player previous entry (playerctl)
-	{ 0,                            XF86XK_AudioPlay,          spawn,          {.v = pctlppcmd } },          // media player pause/play (playerctl)
-	{ 0,                            XF86XK_AudioRaiseVolume,   spawn,          {.v = pctluvolcmd } },        // media player increase volume by .05 (playerctl)
-	{ 0,                            XF86XK_AudioLowerVolume,   spawn,          {.v = pctldvolcmd } },        // media player decrease volume by .05 (playerctl)
-	{ 0,                            XF86XK_AudioMute,          spawn,          {.v = mpcmd } },              // spawn audio player (elisa)
-	{ MODKEY,                       XK_6,                      view,           {.ui = ~0 } },                // duplicate of XK_0
-	{ MODKEY|ShiftMask,             XK_6,                      tag,            {.ui = ~0 } },                // duplicate of XK_0
-	{ MODKEY|ShiftMask,             XK_m,                      togglefullscr,  {0} },                        // toggles fullscreen on a window
-	{ MODKEY|ShiftMask,             XK_backslash,              spawn,          {.v = scale1080 } },          // changes display scaling sorta (xrandr)
-	{ MODKEY|ShiftMask,             XK_bracketright,           spawn,          {.v = scale720 } },           // changes display scaling sorta (xrandr)
-	{ MODKEY|ShiftMask,             XK_apostrophe,             spawn,          {.v = scale540 } },           // changes display scaling sorta (xrandr)
-	{ MODKEY|ShiftMask,             XK_bracketleft,            spawn,          {.v = scale768 } },           // changes display scaling sorta (xrandr)
-	{ MODKEY|ShiftMask,             XK_semicolon,              spawn,          {.v = scale480 } },           // changes display scaling sorta (xrandr)
+	// commands i have added or modified from defaults
+	/* applications */
+	{ MODKEY|ShiftMask,             XK_Return,                 spawn,          {.v = app_terminal } },    // spawn terminal (rxvt-unicode)
+	{ MODKEY|ShiftMask,             XK_f,                      spawn,          {.v = app_webbrowser } },  // spawn browser (palemoon)
+	{ MODKEY|ShiftMask,             XK_g,                      spawn,          {.v = app_filemanager } }, // spawn file manager (pcmanfm)
+	{ MODKEY|ShiftMask,             XK_e,                      spawn,          {.v = app_editor } },      // spawn editor (code)
+	{ MODKEY|ShiftMask,             XK_d,                      spawn,          {.v = app_messenger } },   // spawn instant messenger (discord)
+	{ MODKEY|ShiftMask,             XK_s,                      spawn,          {.v = app_gamestore } },   // spawn game launcher (steam)
+	/* screenshot */
+	{ MODKEY,                       XK_Print,                  spawn,          {.v = screenshot_full } },             // take full screenshot (escrotum)
+	{ MODKEY|ShiftMask,             XK_Print,                  spawn,          {.v = screenshot_select } },           // take screenshot of selection (escrotum)
+	{ MODKEY|ControlMask,           XK_Print,                  spawn,          {.v = screenshot_select_clipboard } }, // take screenshot of selection and only copy to clipboard (escrotum)
+	{ MODKEY|ControlMask|ShiftMask, XK_Print,                  spawn,          {.v = screenshot_select_video } },     // record screen selection, stop with ctl ctrl-alt-s (escrotum)
+	/* media control */
+	{ 0,                            XF86XK_AudioNext,          spawn,          {.v = media_next } },          // media player next entry (playerctl)
+	{ 0,                            XF86XK_AudioPrev,          spawn,          {.v = media_previous } },      // media player previous entry (playerctl)
+	{ 0,                            XF86XK_AudioPlay,          spawn,          {.v = media_playpause } },     // media player pause/play (playerctl)
+	{ 0,                            XF86XK_AudioRaiseVolume,   spawn,          {.v = media_volume_up } },     // media player increase volume by .05 (playerctl)
+	{ 0,                            XF86XK_AudioLowerVolume,   spawn,          {.v = media_volume_down } },   // media player decrease volume by .05 (playerctl)
+	{ 0,                            XF86XK_AudioMute,          spawn,          {.v = media_launch_player } }, // spawn audio player (clementine)
+	/* windowing */
+	{ MODKEY,                       XK_6,                      view,           {.ui = ~0 } }, // duplicate of XK_0
+	{ MODKEY|ShiftMask,             XK_6,                      tag,            {.ui = ~0 } }, // duplicate of XK_0
+	{ MODKEY|ShiftMask,             XK_m,                      togglefullscr,  {0} },         // toggles fullscreen on a window
+	/* resolution */
+	// bilinear
+	//{ MODKEY|ShiftMask,             XK_backslash,              spawn,          {.v = scale1080 } }, // 
+	// nearest
 	/* default keys */
 	{ MODKEY,                       XK_p,                      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_b,                      togglebar,      {0} },
@@ -171,7 +169,7 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = app_terminal } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
