118248229f461b12de73b8d52890015ca89d930e
theo <krueger.theo@gmail.com>
2021-07-17 01:44:41 -0400

    add tb to rules

--- config.h
+++ config.h
@@ -31,10 +31,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class         instance      title   tags mask     isfloating   monitor */
-    {  "discord",    "discord",    NULL,   1,            0,           1 },
-	{  "Steam",      "Steam",      NULL,   1 << 1,       0,           1 },
-	{  "Clementine", "clementine", NULL,   1 << 2,       0,           1 },
+	/* class          instance      title   tags mask     isfloating   monitor */
+    {  "discord",     "discord",    NULL,   1,            0,           1 },
+	{  "Steam",       "Steam",      NULL,   1 << 1,       0,           1 },
+	{  "Clementine",  "clementine", NULL,   1 << 2,       0,           1 },
+	{  "Thunderbird", "Mail",       NULL,   1 << 3,       0,           1 },
 };
 
 /* layout(s) */
