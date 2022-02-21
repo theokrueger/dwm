8e0add6aee180da6acb37ca5e688bff95fe86d79
theokrueger <theokrueger@airmail.cc>
2022-02-20 23:11:00 -0500

    read the config.h

--- config.h
+++ config.h
@@ -32,10 +32,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class          instance      title   tags mask     isfloating   monitor */
-	{NULL, "discord", NULL, 1, 0, 1},
-	{NULL, "Steam", NULL, 1 << 1, 0, 1},
-	{"Clementine", "clementine", NULL, 1 << 2, 0, 1},
-	{"Thunderbird", NULL, NULL, 1 << 3, 0, 1},
+	{NULL, NULL, NULL, 1, 0, 1},
 };
 
 /* layout(s) */
@@ -67,100 +64,54 @@ static const Layout layouts[] = {
 /* commands */
 // note that due to my laziness this is actually suboptimal, having commands execute shell scripts instead would entail more extensibility and ease of modification.
 /* applications */
-static const char *app_terminal[] = {"st", NULL};			  // bound to mod-enter
-#define PRIVATEBROWSERPROFILE "userjs"
-#define SAVEDBROWSERPROFILE "default"
-static const char *app_webbrowser[] = {"firefox", "-P", PRIVATEBROWSERPROFILE, NULL};	  // bound to mod-f
-static const char *app_webbrowser2[] = {"firefox", "-P", SAVEDBROWSERPROFILE, NULL};	  // bound to mod-shift-f
-static const char *app_filemanager[] = {"pcmanfm", NULL};	  // bound to mod-g
-static const char *app_editor[] = {"code", NULL};			  // bound to mod-e
-static const char *app_messenger[] = {"discord", NULL};		  // bound to mod-d
-static const char *app_gamestore[] = {"steam", NULL};		  // bound to mod-s
-static const char *app_emailclient[] = {"thunderbird", NULL}; // bound to mod-t
+static const char *app_terminal[] = {"st", NULL};
+static const char *app_webbrowser[] = {"firefox", NULL};
+static const char *app_webbrowser2[] = {"chromium", NULL};
+static const char *app_filemanager[] = {"pcmanfm", NULL};
+static const char *app_editor[] = {"emacs", NULL};
+static const char *app_emailclient[] = {"thunderbird", NULL};
 /* screenshot */
-static const char *screenshot_select_clipboard[] = {"escrotum", "-s", "-C", NULL};				  // bound to mod-ctrl-s
+static const char *screenshot_select_clipboard[] = {"escrotum", "-s", "-C", NULL};
 /* media control */
-#define MEDIAPLAYER "clementine"
+#define MEDIAPLAYER "strawberry"
 #define XF86XK_AudioNext 0x1008FF17
-static const char *media_next[] = {"playerctl", "-p", MEDIAPLAYER, "next", NULL}; // bound to media next
+static const char *media_next[] = {"playerctl", "-p", MEDIAPLAYER, "next", NULL};
 #define XF86XK_AudioPrev 0x1008FF16
-static const char *media_previous[] = {"playerctl", "-p", MEDIAPLAYER, "previous", NULL}; // bound to media previous
+static const char *media_previous[] = {"playerctl", "-p", MEDIAPLAYER, "previous", NULL};
 #define XF86XK_AudioPlay 0x1008FF14
-static const char *media_playpause[] = {"playerctl", "-p", MEDIAPLAYER, "play-pause", NULL}; // bound to media play/pause
+static const char *media_playpause[] = {"playerctl", "-p", MEDIAPLAYER, "play-pause", NULL};
 #define XF86XK_AudioRaiseVolume 0x1008FF13
-static const char *media_volume_up[] = {"playerctl", "-p", MEDIAPLAYER, "volume", "0.05+", NULL}; // bound to media volume up
+static const char *media_volume_up[] = {"playerctl", "-p", MEDIAPLAYER, "volume", "0.05+", NULL};
 #define XF86XK_AudioLowerVolume 0x1008FF11
-static const char *media_volume_down[] = {"playerctl", "-p", MEDIAPLAYER, "volume", "0.05-", NULL}; // bound to media volume down
+static const char *media_volume_down[] = {"playerctl", "-p", MEDIAPLAYER, "volume", "0.05-", NULL};
 #define XF86XK_AudioMute 0x1008FF12
-static const char *media_launch_player[] = {MEDIAPLAYER, NULL}; // bound to media volume mute
-/* display scaling */
-#define PRIMARYDISPLAY "DP-0"			// display to apply scaling keybinds to, this short script should return string of primary display.
-#define DISPLAYFILTERTYPE "nearest"		// change display scaling filtering. valid types: nearest,bilinear
-// 16:9
-static const char *scale0[] = {"xrandr", "--output", PRIMARYDISPLAY, "--scale-from", "1920x1080", "--filter", DISPLAYFILTERTYPE, NULL}; // bound to mod-ctrl-shift-[qwert]
-static const char *scale1[] = {"xrandr", "--output", PRIMARYDISPLAY, "--scale-from", "1280x720", "--filter", DISPLAYFILTERTYPE, NULL};
-static const char *scale2[] = {"xrandr", "--output", PRIMARYDISPLAY, "--scale-from", "960x540", "--filter", DISPLAYFILTERTYPE, NULL};
-static const char *scale3[] = {"xrandr", "--output", PRIMARYDISPLAY, "--scale-from", "854x480", "--filter", DISPLAYFILTERTYPE, NULL};
-static const char *scale4[] = {"xrandr", "--output", PRIMARYDISPLAY, "--scale-from", "640x360", "--filter", DISPLAYFILTERTYPE, NULL};
-// 4:3
-static const char *scale5[] = {"xrandr", "--output", PRIMARYDISPLAY, "--scale-from", "1440x1080", "--filter", DISPLAYFILTERTYPE, NULL}; // bound to mod-ctrl-shift-[asdfg]
-static const char *scale6[] = {"xrandr", "--output", PRIMARYDISPLAY, "--scale-from", "1280x960", "--filter", DISPLAYFILTERTYPE, NULL};
-static const char *scale7[] = {"xrandr", "--output", PRIMARYDISPLAY, "--scale-from", "1024x768", "--filter", DISPLAYFILTERTYPE, NULL};
-static const char *scale8[] = {"xrandr", "--output", PRIMARYDISPLAY, "--scale-from", "800x600", "--filter", DISPLAYFILTERTYPE, NULL};
-static const char *scale9[] = {"xrandr", "--output", PRIMARYDISPLAY, "--scale-from", "640x480", "--filter", DISPLAYFILTERTYPE, NULL};
-/* display rotation */
-static const char *rot0[] = {"xrandr", "--output", PRIMARYDISPLAY, "--rotate", "normal", NULL}; // bound to mod-ctrl-shift-arrow
-static const char *rot1[] = {"xrandr", "--output", PRIMARYDISPLAY, "--rotate", "inverted", NULL};
-static const char *rot2[] = {"xrandr", "--output", PRIMARYDISPLAY, "--rotate", "left", NULL};
-static const char *rot3[] = {"xrandr", "--output", PRIMARYDISPLAY, "--rotate", "right", NULL};
+static const char *media_launch_player[] = {MEDIAPLAYER, NULL};
 /* demenu commands */
 static char dmenumon[2] = "0";																																				 /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {"dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_pink, "-sf", col_gray4, NULL};				 // launch dmenu
 static const char *app_dmenu_nm[] = {"networkmanager_dmenu", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_pink, "-sf", col_gray4, NULL}; // launch dmenunm
 
 static Key keys[] = {
-	/* added keys */
 	/* applications */
-	{MODKEY, XK_Return, spawn, {.v = app_terminal}},							// spawn terminal (st)
-	{MODKEY | ShiftMask, XK_f, spawn, {.v = app_webbrowser}},					// spawn private browser (firefox)
-	{MODKEY | ControlMask, XK_f, spawn, {.v = app_webbrowser2}},				// spawn public browser (firefox)
-	{MODKEY | ShiftMask, XK_g, spawn, {.v = app_filemanager}},					// spawn file manager (pcmanfm)
-	{MODKEY | ShiftMask, XK_e, spawn, {.v = app_editor}},						// spawn editor (code)
-	{MODKEY | ShiftMask, XK_d, spawn, {.v = app_messenger}},					// spawn instant messenger (discord)
-	{MODKEY | ShiftMask, XK_s, spawn, {.v = app_gamestore}},					// spawn game launcher (steam)
-	{MODKEY | ShiftMask, XK_t, spawn, {.v = app_emailclient}},					// spawn email client (thunderbird)
-	{MODKEY | ShiftMask, XK_o, spawn, {.v = app_dmenu_nm}},						// spawn networkmanager_dmenu
-	/* screenshot */
-	{MODKEY | ControlMask, XK_s, spawn, {.v = screenshot_select_clipboard}},	// take screenshot of selection and only copy to clipboard (escrotum)
+	{MODKEY, XK_Return, spawn, {.v = app_terminal}},
+	{MODKEY | ShiftMask, XK_f, spawn, {.v = app_webbrowser}},
+	{MODKEY | ControlMask, XK_f, spawn, {.v = app_webbrowser2}},
+	{MODKEY | ShiftMask, XK_g, spawn, {.v = app_filemanager}},
+	{MODKEY | ShiftMask, XK_e, spawn, {.v = app_editor}},
+	{MODKEY | ShiftMask, XK_t, spawn, {.v = app_emailclient}},
+	{MODKEY | ShiftMask, XK_o, spawn, {.v = app_dmenu_nm}},
+	{MODKEY | ControlMask, XK_s, spawn, {.v = screenshot_select_clipboard}},
 	/* media control */
-	{0, XF86XK_AudioNext, spawn, {.v = media_next}},				// media player next entry (playerctl)
-	{0, XF86XK_AudioPrev, spawn, {.v = media_previous}},			// media player previous entry (playerctl)
-	{0, XF86XK_AudioPlay, spawn, {.v = media_playpause}},			// media player pause/play (playerctl)
-	{0, XF86XK_AudioRaiseVolume, spawn, {.v = media_volume_up}},	// media player increase volume by .05 (playerctl)
-	{0, XF86XK_AudioLowerVolume, spawn, {.v = media_volume_down}},	// media player decrease volume by .05 (playerctl)
-	{0, XF86XK_AudioMute, spawn, {.v = media_launch_player}},		// spawn audio player (clementine)
+	{0, XF86XK_AudioNext, spawn, {.v = media_next}},
+	{0, XF86XK_AudioPrev, spawn, {.v = media_previous}},
+	{0, XF86XK_AudioPlay, spawn, {.v = media_playpause}},
+	{0, XF86XK_AudioRaiseVolume, spawn, {.v = media_volume_up}},
+	{0, XF86XK_AudioLowerVolume, spawn, {.v = media_volume_down}},
+	{0, XF86XK_AudioMute, spawn, {.v = media_launch_player}},
 	/* windowing */
 	{MODKEY, XK_7, view, {.ui = ~0}},				// duplicate of XK_0
 	{MODKEY | ShiftMask, XK_7, tag, {.ui = ~0}},	// duplicate of XK_0
 	{MODKEY | ShiftMask, XK_m, togglefullscr, {0}},	// toggles fullscreen on a window
-	/* resolution */
-	// 19:9
-	{MODKEY | ControlMask | ShiftMask, XK_q, spawn, {.v = scale0}},	// req xrandr
-	{MODKEY | ControlMask | ShiftMask, XK_w, spawn, {.v = scale1}},
-	{MODKEY | ControlMask | ShiftMask, XK_e, spawn, {.v = scale2}},
-	{MODKEY | ControlMask | ShiftMask, XK_r, spawn, {.v = scale3}},
-	{MODKEY | ControlMask | ShiftMask, XK_t, spawn, {.v = scale4}},
-	// 4:3
-	{MODKEY | ControlMask | ShiftMask, XK_a, spawn, {.v = scale5}},
-	{MODKEY | ControlMask | ShiftMask, XK_s, spawn, {.v = scale6}},
-	{MODKEY | ControlMask | ShiftMask, XK_d, spawn, {.v = scale7}},
-	{MODKEY | ControlMask | ShiftMask, XK_f, spawn, {.v = scale8}},
-	{MODKEY | ControlMask | ShiftMask, XK_g, spawn, {.v = scale9}},
-	/* rotation */
-	{MODKEY | ControlMask | ShiftMask, XK_Up, spawn, {.v = rot0}},	// req xrandr
-	{MODKEY | ControlMask | ShiftMask, XK_Down, spawn, {.v = rot1}},
-	{MODKEY | ControlMask | ShiftMask, XK_Left, spawn, {.v = rot2}},
-	{MODKEY | ControlMask | ShiftMask, XK_Right, spawn, {.v = rot3}},
 	/* default keys */
 	{MODKEY, XK_p, spawn, {.v = dmenucmd}},
 	{MODKEY, XK_b, togglebar, {0}},
